a3d1d826f488f0acca0c110ca029daf64057a301
aryanojhaa <2k23.csds2311504@gmail.com>
2025-01-29 21:57:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ const quotes = [
   "Every night I fall asleep dreaming of you, and every morning I wake up thinking of you.",
   "As the stars twinkle in the night sky, my heart twinkles with thoughts of you.",
   "The moon is beautiful tonight, but not as beautiful as you.",
-  "Distance may separate our bodies, but never our hearts. Good night, my love.",
   "Like the stars that light up the dark sky, you light up my world.",
-  "Sweet dreams, Mansi. Until tomorrow, keep my love in your heart.",
-  "The day is over, but my love for you never ends. Good night, my angel.",
+  "Sweet dreams, Mansi."
+  "The day is over, but my love for you never ends. Good night"
   "As you drift off to sleep, know that you're the last thought on my mind.",
-  "May your dreams be as sweet as you are. Good night, my beloved.",
+  "May your dreams be as sweet as you are. Good night",
   "The night is more beautiful because you exist in my world.",
 ];
 
